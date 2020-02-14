39c55859e46e2979474f4a8acd1e395b8ea964fe
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-02-14 19:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ class Circle{
   }
   get area(){
     let a = Math.pow(this.radius ,2) * Math.PI
+    return a
   }
   set diameter( number){
      this.radius = number /2
   }
   set circumference(number){
     this.radius = number /(2 * Math.PI)
+  }
+  set area(number){
+    this.radius = Math.sqrt(number/Math.PI)
   }
 }
